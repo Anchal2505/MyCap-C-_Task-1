a1c51a8e6af003bb5ed01b33e35c5bff650d03c6
Anchal2505 <91790231+Anchal2505@users.noreply.github.com>
2021-12-06 19:45:50 +0530

    Add files via upload

---
+++ MyCap-Task-1.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a , b, c;
+    cout<<"\nenter first number: ";
+    cin>>a;
+    cout<<"\nenter second number: ";
+    cin>>b;
+    cout<<"\nmultiplication of given two numbers is: ";
+    c=a*b;
+    cout<<c;
+    return 0;
+}
